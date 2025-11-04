c76b9fe06c9326ee8d8e1d1ff159fbe52763353c
Rajesh Karri <rajeshkarri827@gmail.com>
2025-11-04 06:21:32 +0000

    Added Fragments

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -1,10 +1,67 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/core/Fragment"
+], function (Controller, JSONModel, Fragment) {
+    
+  "use strict";
 
-    return Controller.extend("naruto.helperbox.controller.View1", {
-        onInit() {
-        }
-    });
+  return Controller.extend("naruto.helperbox.controller.View1", {
+
+    onInit: function () {
+      var oModel = new JSONModel("../model/data.json");
+      this.getView().setModel(oModel);
+    },
+
+
+    onValueHelpName: function () {
+      var oView = this.getView();
+
+      if (!this._pNameDialog) {
+        this._pNameDialog = Fragment.load({
+          id: oView.getId(),
+          name: "naruto.helperbox.view.NameValueHelp", 
+          controller: this
+        }).then(function (oDialog) {
+          oView.addDependent(oDialog);
+          return oDialog;
+        });
+      }
+
+      this._pNameDialog.then(function (oDialog) {
+        oDialog.open();
+      });
+    },
+
+    onConfirmName: function (oEvent) {
+      var sSelectedName = oEvent.getParameter("selectedItem").getTitle();
+      this.getView().byId("inpName").setValue(sSelectedName);
+    },
+
+
+    onValueHelpRole: function () {
+      var oView = this.getView();
+
+      if (!this._pRoleDialog) {
+        this._pRoleDialog = Fragment.load({
+          id: oView.getId(),
+          name: "naruto.helperbox.view.RoleValueHelp", 
+          controller: this
+        }).then(function (oDialog) {
+          oView.addDependent(oDialog);
+          return oDialog;
+        });
+      }
+
+      this._pRoleDialog.then(function (oDialog) {
+        oDialog.open();
+      });
+    },
+
+    onConfirmRole: function (oEvent) {
+      var sSelectedRole = oEvent.getParameter("selectedItem").getTitle();
+      this.getView().byId("inpRole").setValue(sSelectedRole);
+    }
+
+  });
 });
