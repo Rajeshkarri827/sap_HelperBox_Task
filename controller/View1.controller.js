34e60313f59e4ca73c2c4d9b51def17051ed5a0a
Rajesh Karri <rajeshkarri827@gmail.com>
2025-11-04 10:01:42 +0000

    Filterfunction Added

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -1,18 +1,18 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel",
-  "sap/ui/core/Fragment"
-], function (Controller, JSONModel, Fragment) {
-    
+  "sap/ui/core/Fragment",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Fragment, Filter, FilterOperator) {
   "use strict";
 
   return Controller.extend("naruto.helperbox.controller.View1", {
 
     onInit: function () {
-      var oModel = new JSONModel("../model/data.json");
+      var oModel = new JSONModel("model/data.json");
       this.getView().setModel(oModel);
     },
-
 
     onValueHelpName: function () {
       var oView = this.getView();
@@ -20,7 +20,7 @@ sap.ui.define([
       if (!this._pNameDialog) {
         this._pNameDialog = Fragment.load({
           id: oView.getId(),
-          name: "naruto.helperbox.view.NameValueHelp", 
+          name: "naruto.helperbox.view.NameValueHelp",
           controller: this
         }).then(function (oDialog) {
           oView.addDependent(oDialog);
@@ -38,6 +38,14 @@ sap.ui.define([
       this.getView().byId("inpName").setValue(sSelectedName);
     },
 
+  
+    onSearchName: function (oEvent) {
+      var sQuery = oEvent.getParameter("value");
+      var oFilter = new Filter("name", FilterOperator.Contains, sQuery);
+      var oBinding = oEvent.getSource().getBinding("items");
+      oBinding.filter(sQuery ? [oFilter] : []);
+    },
+
 
     onValueHelpRole: function () {
       var oView = this.getView();
@@ -45,7 +53,7 @@ sap.ui.define([
       if (!this._pRoleDialog) {
         this._pRoleDialog = Fragment.load({
           id: oView.getId(),
-          name: "naruto.helperbox.view.RoleValueHelp", 
+          name: "naruto.helperbox.view.RoleValueHelp",
           controller: this
         }).then(function (oDialog) {
           oView.addDependent(oDialog);
@@ -61,6 +69,14 @@ sap.ui.define([
     onConfirmRole: function (oEvent) {
       var sSelectedRole = oEvent.getParameter("selectedItem").getTitle();
       this.getView().byId("inpRole").setValue(sSelectedRole);
+    },
+
+  
+    onSearchRole: function (oEvent) {
+      var sQuery = oEvent.getParameter("value");
+      var oFilter = new Filter("role", FilterOperator.Contains, sQuery);
+      var oBinding = oEvent.getSource().getBinding("items");
+      oBinding.filter(sQuery ? [oFilter] : []);
     }
 
   });
